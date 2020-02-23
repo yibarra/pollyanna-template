fb85c02ff4de8b78e7dc0641cdf33cd04e1c9c0c
Julian Ibarra <yeissonibarra@gmail.com>
2020-02-23 17:51:27 +0000

    Updates

---
+++ precache-manifest.aa754b787ea89a00108e52cd381eb837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432c86be7c41ce92da5026208cabd5bf",
+    "url": "/pollyanna-template/index.html"
+  },
+  {
+    "revision": "09a44e04e2f02c056e28",
+    "url": "/pollyanna-template/static/css/main.5b2af825.chunk.css"
+  },
+  {
+    "revision": "87d33ae91c29e47a7703",
+    "url": "/pollyanna-template/static/js/2.b467f88d.chunk.js"
+  },
+  {
+    "revision": "6bfe307b740e17ac7a96205ffecc15d9",
+    "url": "/pollyanna-template/static/js/2.b467f88d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09a44e04e2f02c056e28",
+    "url": "/pollyanna-template/static/js/main.6eb82321.chunk.js"
+  },
+  {
+    "revision": "a8739a935202a176f3cb",
+    "url": "/pollyanna-template/static/js/runtime-main.5f3f7a98.js"
+  }
+]);
